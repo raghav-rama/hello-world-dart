2a35cb06f3954ab76b0ebd50e1469c36a97b7519
raghav-rama <hackerer528@gmail.com>
2022-08-19 08:05:09 +0530

    use the dart approach to write long strings

--- lib/string_literals.dart
+++ lib/string_literals.dart
@@ -1,6 +1,12 @@
 void stringLiterals() {
     /**
-    String = 'Single'
-    String = "Double"
+    String p = 'Single'
+    String q = "Double"
+    A very long string
+    String r = 'a long string'
+                'continuation'
+                'continuation'
+                '...'
+    All this is combined a one string literal in dart
     */
 }
