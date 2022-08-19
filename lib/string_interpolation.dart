069bde229e4632b417ebeb43614b169142d813bd
raghav-rama <hackerer528@gmail.com>
2022-08-19 08:05:29 +0530

    add string interpolation

---
+++ lib/string_interpolation.dart
@@ -0,0 +1,15 @@
+void stringInterpolation() {
+    /**
+    String name = 'Kevin'
+    Normal:
+    String s = 'My name is' + name;
+    Using string interpolation:
+    String s = 'My name is $name';
+
+    int l = 20;
+    int b = 10;
+    print(The sum of $l and $b is ${l+b});
+    print("The length of String s is $(name.length)");
+    print("")
+    */
+}
